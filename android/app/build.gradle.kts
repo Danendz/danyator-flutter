df4d2f4e1ea55686035a31765ac4cda18feefa0b
danila <danilapayne@gmail.com>
2025-02-20 09:07:29 +0500

    minimal working example

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.danya.danyator.danyator"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.0.13004108"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
